dbbea3b024e4d2a8e5cbb4794cda6693cdeef55c
None <None>
2019-06-25 14:06:30 -0700

    Final

--- b.py
+++ b.py
@@ -19,48 +19,59 @@ from time import sleep
 #RTT varia entre 16ms e 324ms
 #2 fluxos com mesmo RTT
 
-class Router( Node ):
-    def config( self, **params):
-        super( Router, self).config( **params )
-        self.cmd( 'sysctl net.ipv4.ip_foward=1' )
-    
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
     def terminate( self ):
-        self.cmd( 'sysctl net.ipv4.ip_foward=0' )
-        super( Router, self).terminate()
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
+
 
 class MyTopo( Topo ):
     
     def build( self, **_opts ):
 
-        defaultIP1 = '10.10.5.1/24' #IP pro roteador 1
-        defaultIP2 = '10.10.5.2/24' #IP pro roteador 2
+        defaultIP1 = '10.0.5.1/24' #IP pro roteador 1
+        
         #s1 = self.addSwitch( 's1', cls=LinuxBridge)
 
-        router1 = self.addNode( 'r1', cls=Router, ip=defaultIP1)
-        router2 = self.addNode( 'r2', cls=Router, ip=defaultIP2)
-
-        client1 = self.addHost( 'h1', ip='10.10.1.1/24', defaultRoute='via 10.10.1.2' )
-        client2 = self.addHost( 'h2', ip='10.10.2.1/24', defaultRoute='via 10.10.2.2' )
-        client3 = self.addHost( 'h3', ip='10.10.3.1/24', defaultRoute='via 10.10.3.2' )
-        client4 = self.addHost( 'h4', ip='10.10.4.1/24', defaultRoute='via 10.10.4.2' )
-
-        server1 = self.addHost( 'server1', ip='10.10.6.2/24', defaultRoute='via 10.10.6.1' )
-        server2 = self.addHost( 'server2', ip='10.10.7.2/24', defaultRoute='via 10.10.7.1' )
-        server3 = self.addHost( 'server3', ip='10.10.8.2/24', defaultRoute='via 10.10.8.1' )
-        server4 = self.addHost( 'server4', ip='10.10.9.2/24', defaultRoute='via 10.10.9.1' )
-
-
-        self.addLink(router1, router2, intfName1='r1-eth1', params1={'ip':defaultIP1},intfName2='r2-eth1', params2={'ip':defaultIP2})
-        self.addLink(client1, router1, intfName2='r1-eth2', params2={ 'ip': '10.10.1.2/24'} )
-        self.addLink(client2, router1, intfName2='r1-eth5', params2={ 'ip': '10.10.2.2/24'} )
-        self.addLink(client3, router1, intfName2='r1-eth3', params2={ 'ip': '10.10.3.2/24'} )
-        self.addLink(client4, router1, intfName2='r1-eth4', params2={ 'ip': '10.10.4.2/24'} )
+        router1 = self.addNode( 'r1', cls=LinuxRouter, ip=defaultIP1, defaultRoute='via 10.0.6.2')
         
 
-        self.addLink(server1, router2, intfName2='r2-eth5', params2={ 'ip': '10.10.6.1/24'} )
-        self.addLink(server2, router2, intfName2='r2-eth2', params2={ 'ip': '10.10.7.1/24'} )
-        self.addLink(server3, router2, intfName2='r2-eth4', params2={ 'ip': '10.10.8.1/24'} )
-        self.addLink(server4, router2, intfName2='r2-eth3', params2={ 'ip': '10.10.9.1/24'} )
+        client1 = self.addHost( 'h1', ip='10.0.5.10/24', defaultRoute='via 10.0.5.1' )
+        client2 = self.addHost( 'h2', ip='10.0.4.10/24', defaultRoute='via 10.0.4.1' )
+        client3 = self.addHost( 'h3', ip='10.0.3.10/24', defaultRoute='via 10.0.3.1' )
+        client4 = self.addHost( 'h4', ip='10.0.2.10/24', defaultRoute='via 10.0.2.1' )
+
+        self.addLink(client1, router1, intfName2='r1-eth1', params2={ 'ip': '10.0.5.1/24'} )
+        self.addLink(client2, router1, intfName2='r1-eth2', params2={ 'ip': '10.0.4.1/24'} )
+        self.addLink(client3, router1, intfName2='r1-eth3', params2={ 'ip': '10.0.3.1/24'} )
+        self.addLink(client4, router1, intfName2='r1-eth4', params2={ 'ip': '10.0.2.1/24'} )
+        
+
+
+        
+
+
+        defaultIP2 = '10.0.6.2/24' #IP pro roteador 2
+        router2 = self.addNode( 'r2', cls=LinuxRouter, ip=defaultIP2, defaultRoute='via 10.0.6.1')
+        self.addLink(router2, router1, intfName2='r1-eth5', params2={'ip':'10.0.6.1/24'})
+        #,intfName2='r2-eth1', params2={'ip':defaultIP2}
+
+        server1 = self.addHost( 'server1', ip='10.0.10.100/24', defaultRoute='via 10.0.10.1' )
+        server2 = self.addHost( 'server2', ip='10.0.9.110/24', defaultRoute='via 10.0.9.1' )
+        server3 = self.addHost( 'server3', ip='10.0.8.130/24', defaultRoute='via 10.0.8.1' )
+        server4 = self.addHost( 'server4', ip='10.0.7.140/24', defaultRoute='via 10.0.7.1' )
+       
+        self.addLink(server1, router2, intfName2='r2-eth1', params2={ 'ip': '10.0.10.1/24'} )
+        self.addLink(server2, router2, intfName2='r2-eth2', params2={ 'ip': '10.0.9.1/24'} )
+        self.addLink(server3, router2, intfName2='r2-eth3', params2={ 'ip': '10.0.8.1/24'} )
+        self.addLink(server4, router2, intfName2='r2-eth4', params2={ 'ip': '10.0.7.1/24'} )
 
         # Each host gets 50%/n of system CPU
         #host = self.addHost( 'h%s' % (h + 1), cpu=.5/n )
